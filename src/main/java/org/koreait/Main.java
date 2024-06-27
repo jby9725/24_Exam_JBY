068442533c1389b6c28bd79bbd65d4ec431ca1f3
jby9725 <jby9725@gmail.com>
2024-06-27 13:38:50 +0900

    1번 문제 풀이

--- src/main/java/org/koreait/Main.java
+++ src/main/java/org/koreait/Main.java
@@ -1,7 +1,24 @@
 package org.koreait;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int x;
+        int y;
+
+        Scanner input = new Scanner(System.in);
+
+        x = input.nextInt();
+        y = input.nextInt();
+
+        for(int i = 0; i<y; i++)
+        {
+            for(int j = 0; j<x; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
     }
 }
